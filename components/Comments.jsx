05f874d6e9b2bb7a36ede2162fd714405dd7c880
justinshin6 <justinshin@brown.edu>
2022-07-13 22:08:03 -0400

    update comments

--- components/Comments.jsx
+++ components/Comments.jsx
@@ -22,7 +22,8 @@ const Comments = ({ slug }) => {
           <h3 className="text-xl mb-8 font-semibold border-b pb-4">
             {comments.length}
             {' '}
-            Comments
+            {comments.length === 1 ? 'Comment' : 'Comments'}
+
           </h3>
             {comments.map((comment, index) => (
               <div key={index} className="border-b border-gray-100 mb-4 pb-4">
