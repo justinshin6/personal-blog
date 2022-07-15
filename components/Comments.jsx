6db7bf0f41d76a529d9447c9bc91747e74332d42
justinshin6 <justinshin@brown.edu>
2022-07-14 22:03:41 -0400

    update comments

--- components/Comments.jsx
+++ components/Comments.jsx
@@ -20,21 +20,23 @@ const Comments = ({ slug }) => {
       {comments.length > 0 && (
         <div className="bg-gray-700 bg-opacity-30 shadow-lg rounded-lg p-8 pb-12 mb-8">
           <h3 className="text-xl mb-8 font-semibold border-b pb-4">
+            <span className="text-white">
             {comments.length}
             {' '}
             {comments.length === 1 ? 'Comment' : 'Comments'}
+            </span>
 
           </h3>
             {comments.map((comment, index) => (
               <div key={index} className="border-b border-gray-100 mb-4 pb-4">
-                <p className="mb-4">
-                  <span className="font-semibold">{comment.name}</span>
+                <p className="mb-4 text-white">
+                  <span className="font-semibold text-white">{comment.name}</span>
                   {' '}
                   on
                   {' '}
                   {moment(comment.createdAt).format('MMM DD, YYYY')}
                 </p>
-                <p className="whitespace-pre-line text-gray-600 w-full">{parse(comment.comment)}</p>
+                <p className="whitespace-pre-line text-white w-full">{parse(comment.comment)}</p>
               </div>
             ))}
         </div>
