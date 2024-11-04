76594f7ed57c49dbabfaf49ec9a7d356c5f41091
justinshin6 <justinshin@brown.edu>
2024-11-03 23:27:51 -0500

    change footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
+import React from 'react';
+
 const Footer = () => {
+    const currentYear = new Date().getFullYear();
+
     return (
         <div className="container mx-auto px-10 mb-8">
-      <div className="border-t w-full inline-block border-sky-400 py-8	">
-        <div className="text-center block">
-            <span className="text-white">Made by Justin Shin 2022</span>
+            <div className="border-t w-full inline-block border-sky-400 py-8">
+                <div className="text-center block">
+                    <span className="text-white">Justin Shin {currentYear}</span>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-    )
+    );
 }
 
-export default Footer
+export default Footer;
